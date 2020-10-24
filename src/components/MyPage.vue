cde3fd2c039e8295103ea510cd6f88e46c0b2aab
S.S <m16072@g.metro-cit.ac.jp>
2020-10-24 15:27:36 +0900

    Profile 変更処理の追加、フロントのicon変更作成 ToDo: apiでimage upload

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-    <div class="row border">
+    <div class="row frame">
       <img :src="img" class="col" id="icon">
       <div class="col" id="summary">
         <div class="inline">
@@ -63,13 +63,24 @@
         </div>
       </div>
       <div class="row" style="margin-top: 15px;">
-        <img :src="img" class="col" id="icon" style="cursor: pointer;" v-on:click="icon">
+        <div style="min-width: 150px;">
+          <div style="min-width: 150px;max-width: 150px;max-height: 150px;min-height: 150px;position: absolute;z-index: 1;">
+           <svg style="min-width: 30px;max-width: 30px;margin-left: 55px;margin-top: 55px;position: absolute;stroke: darkgray;background: rgba(0 0 0 / 0.5);border-radius: 5px;" viewBox="0 0 24 24"><g><path d="M19.708 22H4.292C3.028 22 2 20.972 2 19.708V7.375C2 6.11 3.028 5.083 4.292 5.083h2.146C7.633 3.17 9.722 2 12 2c2.277 0 4.367 1.17 5.562 3.083h2.146C20.972 5.083 22 6.11 22 7.375v12.333C22 20.972 20.972 22 19.708 22zM4.292 6.583c-.437 0-.792.355-.792.792v12.333c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V7.375c0-.437-.355-.792-.792-.792h-2.45c-.317.05-.632-.095-.782-.382-.88-1.665-2.594-2.7-4.476-2.7-1.883 0-3.598 1.035-4.476 2.702-.16.302-.502.46-.833.38H4.293z"></path><path d="M12 8.167c-2.68 0-4.86 2.18-4.86 4.86s2.18 4.86 4.86 4.86 4.86-2.18 4.86-4.86-2.18-4.86-4.86-4.86zm2 5.583h-1.25V15c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-1.25H10c-.414 0-.75-.336-.75-.75s.336-.75.75-.75h1.25V11c0-.414.336-.75.75-.75s.75.336.75.75v1.25H14c.414 0 .75.336.75.75s-.336.75-.75.75z"></path></g></svg>
+           <input v-on:change="onImgChange" accept="image/jpeg,image/png,image/webp" type="file" style="z-index: -1;opacity: 0;margin-left: -70px;margin-top: 50px;width: 0.1px;height: 0.1px;cursor: pointer;"/>
+        </div>
+          <div style="z-index: 2;">
+            <img v-show="showicon" :src="img" class="col" id="icon" style="position: absolute;">
+            <img v-show="!showicon" :src="eimg" class="col" id="icon" style="position: absolute;">
+          </div>
+      </div>
+      <div style="margin-top: 15px;margin-left: 15px;z-index: 1;">
         <form>
           <p>名前</p>
-          <input type="text"/>
+          <input type="text" class="input" v-model="ename"/>
           <p style="margin-top: 10px;">自己紹介</p>
-          <input type="text" style="height: 100%;"/>
+          <textarea v-model="eprofile" class="input" style="height: 100%;resize: none;min-height: 100px;max-height: 100px;"></textarea>
         </form>
+        </div>
       </div>
     </div>
   </div>
@@ -82,14 +93,30 @@ export default {
   name: 'Mypage',
   data () {
     return {
+      showicon: true,
       showContent: false,
       userid: 'test',
       name: 'test',
       profile: 'test profile',
-      img: 'https://pbs.twimg.com/profile_images/1238034989108711424/gQ1_2iGb_400x400.jpg'
+      img: 'https://pbs.twimg.com/profile_images/1238034989108711424/gQ1_2iGb_400x400.jpg',
+      ename: '',
+      eprofile: '',
+      eimg: ''
     }
   },
   methods: {
+    onImgChange (e) {
+      const files = e.target.files || e.dataTransfer.files
+      this.createImage(files[0])
+    },
+    createImage (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        this.eimg = e.target.result
+      }
+      reader.readAsDataURL(file)
+      this.showicon = false
+    },
     openModal () {
       this.showContent = true
     },
@@ -104,19 +131,24 @@ export default {
         withCredentials: true
       }).then(res => {
         this.userid = res.data.userid
-        this.name = res.data.name
-        this.profile = res.data.profile
-        if (res.data.img !== '') this.img = res.data.img
+        this.ename = this.name = res.data.name
+        this.eprofile = this.profile = res.data.profile
+        if (res.data.img !== '') this.eimg = this.img = res.data.img
         console.log(res.data.name)
       }).catch(err => {
         console.log(err)
       })
     },
     edit () {
-      console.log('edit profile')
-    },
-    icon () {
-      console.log('edit icon')
+      var params = new URLSearchParams()
+      params.append('name', this.ename)
+      if (!this.eprofile || !this.eprofile.match(/\S/g)) this.eprofile = ''
+      params.append('profile', this.eprofile)
+      axios.post(process.env.VUE_APP_API_URL_BASE + 'user/edit', params, { withCredentials: true }).then(res => {
+        this.$router.go()
+      }).catch(error => {
+        console.log(error)
+      })
     }
   },
   beforeMount () {
@@ -126,20 +158,25 @@ export default {
 </script>
 
 <style scoped>
+.frame {
+  border-bottom: 1px solid #dee2e6;
+  margin-right: 15px;
+  margin-left: 15px;
+  padding-bottom: 15px;
+}
 #icon {
   border-radius: 9999px;
   width: 15%;
-  min-width: 49px;
   position: relative;
   max-width: 150px;
+  min-width: 150px;
+  max-height: 150px;
+  min-height: 150px;
 }
 .inline {
   display: flex;
   font-size: x-large;
   width: 100%;
-}
-.border {
-  border-bottom: 1px solid #dee2e6;
 }
 #summary {
   padding-left: 0px;
@@ -203,9 +240,10 @@ export default {
   font-weight: 600;
   margin-bottom: 0px;
 }
-input {
+.input {
   width: 150%;
   border: 1px solid;
   border-radius: 5px;
+  margin-left: 15px;
 }
 </style>
